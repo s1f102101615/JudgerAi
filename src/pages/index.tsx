e9dfa7f2675c486a1ae07ec810bfdebf99b1d778
s1f102101615 <s1f102101615@iniad.org>
2023-08-30 22:17:14 +0900

    プロトタイプの作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,57 +1,70 @@
+import dotenv from 'dotenv';
+import openai from 'openai';
+import type { SetStateAction } from 'react';
+import { useEffect, useState } from 'react';
 import styles from './index.module.css';
 
+dotenv.config();
+
+type Message = {
+  author: string;
+  message: string;
+};
+
 const Home = () => {
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (event: { target: { value: SetStateAction<string> } }) => {
+    setInputValue(event.target.value);
+  };
+
+  useEffect(() => {
+    const apikeyer = () => {
+      // .env fileからAPIキーを取得
+      const apiKey = process.env.OPENAI_API_KEY;
+      if (apiKey === null) return;
+      // OpenAIのクライアントを作成
+      const client = new openai.OpenAI({ apiKey });
+      console.log('b', client);
+    };
+    apikeyer();
+  }, []);
+
+  const handleSendMessage = async () => {
+    try {
+      // const response = await client.completions.create({
+      //   model: 'text-davinci-003',
+      //   prompt: `User: ${inputValue}\nAI:`,
+      //   max_tokens: 50,
+      //   n: 1,
+      //   stop: '\n',
+      //   temperature: 0.5,
+      // });
+
+      // const message = response.choices[0].text.trim();
+      const message = 'Hello World';
+      setMessages([...messages, { author: 'them', message }]);
+      setInputValue('');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code} style={{ backgroundColor: '#fafafa' }}>
-            pages/index.js
-          </code>
-        </p>
-
-        <div className={styles.grid}>
-          <a className={styles.card} href="https://nextjs.org/docs">
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a className={styles.card} href="https://nextjs.org/learn">
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a className={styles.card} href="https://github.com/vercel/next.js/tree/master/examples">
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            className={styles.card}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <img src="vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+    <div className={styles.chatcontainer}>
+      <div className={styles.chatmessages}>
+        {messages.map((message, index) => (
+          <div className={styles.chatmessage} key={index}>
+            <strong>{message.author}: </strong>
+            <span>{message.message}</span>
+          </div>
+        ))}
+      </div>
+      <div className={styles.chatinput}>
+        <input type="text" value={inputValue} onChange={handleInputChange} />
+        <button onClick={handleSendMessage}>Send</button>
+      </div>
     </div>
   );
 };
