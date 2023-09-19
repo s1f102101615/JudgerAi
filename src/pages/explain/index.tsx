cac1543725c0baeb9299bd000aa288b6107e9f48
s1f102101615 <s1f102101615@iniad.org>
2023-09-19 20:43:42 +0900

    detail

--- src/pages/explain/index.tsx
+++ src/pages/explain/index.tsx
@@ -5,7 +5,16 @@ function Explain() {
   return (
     <div className={styles.explaincontainer}>
       <div className={styles.h1}>どのような聞き方をすればよいのか</div>
-      <div className={styles.p}>使い方の説明</div>
+      <div className={styles.pow}>使い方の説明</div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
       <a href="/detail">
         <button className={styles.btn}>もっと詳しく</button>
       </a>
