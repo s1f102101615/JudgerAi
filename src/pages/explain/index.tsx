04a09d16b3f75e698000f3a14540413a0df00954
s1f102101615 <s1f102101615@iniad.org>
2023-09-19 19:05:32 +0900

    説明ページ

--- src/pages/explain/index.tsx
+++ src/pages/explain/index.tsx
@@ -1,11 +1,12 @@
 import './index.module.css';
+import styles from './index.module.css';
 
 function Explain() {
   return (
-    <div className="explaincontainer">
-      <div className="h1">説明ページ</div>
-      <div className="p">ここは説明ページです。</div>
-      <button className="btn">もっと詳しく</button>
+    <div className={styles.explaincontainer}>
+      <div className={styles.h1}>どのような聞き方をすればよいのか</div>
+      <div className={styles.p}>ここは説明ページです。</div>
+      <button className={styles.btn}>もっと詳しく</button>
     </div>
   );
 }
