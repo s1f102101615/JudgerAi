a5b1141b524ea8d624710959f107ef19336d6e3a
s1f102101615 <s1f102101615@iniad.org>
2023-09-19 19:09:58 +0900

    detail

--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -1,10 +1,10 @@
 import './index.module.css';
 import styles from './index.module.css';
 
-function Explain() {
+function Detail() {
   return (
     <div className={styles.explaincontainer}>
-      <div className={styles.h1}>どのような聞き方をすればよいのか</div>
+      <div className={styles.h1}>中身で何が起きているのか</div>
       <div className={styles.p}>使い方の説明</div>
       <a href="/detail">
         <button className={styles.btn}>もっと詳しく</button>
@@ -13,4 +13,4 @@ function Explain() {
   );
 }
 
-export default Explain;
+export default Detail;
